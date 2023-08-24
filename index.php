e454627e31267a6289fa3eb1c73743e048638ed2
Artem Zolin <artem@artzolin.ru>
2023-08-25 02:27:13 +0400

    1.0.1
    First commit. Start with added sortable costomizer control

---
+++ index.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to disdisplay a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package aesthetix
+ */
+
+get_header();
+
+if ( get_aesthetix_options( 'sidebar_left_display' ) ) {
+	get_sidebar();
+} ?>
+
+	<main id="primary" <?php aesthetix_content_area_classes(); ?> role="main">
+
+		<?php do_action( 'aesthetix_before_index_page' ); ?>
+
+		<?php if ( have_posts() ) : ?>
+			<?php $i = 0; ?>
+
+			<?php if ( is_home() && ! is_front_page() ) { ?>
+				<header class="content-area-header" aria-label="<?php _e( 'Archive page header', 'aesthetix' ); ?>">
+					<h1 class="content-area-title"><?php single_post_title(); ?></h1>
+				</header>
+			<?php } ?>
+
+			<section class="content-area-content" aria-label="<?php _e( 'Archive page content', 'aesthetix' ); ?>">
+				<div <?php aesthetix_archive_page_columns_wrapper_classes(); ?>>
+
+					<?php while ( have_posts() ) : ?>
+						<?php the_post(); ?>
+
+						<div <?php aesthetix_archive_page_columns_classes( $i ); ?>>
+
+							<?php
+								// Get a template with a post type, if there is one in the theme.
+								if ( file_exists( get_theme_file_path( 'templates/archive/archive-content-type-' . get_post_type() . '.php' ) ) ) {
+									get_template_part( 'templates/archive/archive-content-type', get_post_type(), array( 'counter' => $i ) );
+								} elseif ( get_aesthetix_options( 'archive_' . get_post_type() . '_template_type' ) ) {
+									get_template_part( 'templates/archive/archive-content-type', get_aesthetix_options( 'archive_' . get_post_type() . '_template_type' ), array( 'counter' => $i ) );
+								} else {
+									get_template_part( 'templates/archive/archive-content-type', 'tils', array( 'counter' => $i ) );
+								}
+							?>
+
+						</div>
+
+					<?php endwhile; ?>
+
+				</div>
+			</section>
+
+			<footer class="content-area-footer" aria-label="<?php _e( 'Archive page footer', 'aesthetix' ); ?>">
+				<?php get_template_part( 'templates/archive/archive', 'pagination' ); ?>
+			</footer>
+
+		<?php else : ?>
+
+			<?php get_template_part( 'templates/archive/archive-content', 'none' ); ?>
+
+		<?php endif; ?>
+
+		<?php do_action( 'aesthetix_after_index_page' ); ?>
+
+	</main>
+
+<?php
+
+if ( get_aesthetix_options( 'sidebar_left_display' ) && get_aesthetix_options( 'sidebar_right_display' ) ) {
+	get_sidebar( 'right' );
+} elseif ( get_aesthetix_options( 'sidebar_right_display' ) ) {
+	get_sidebar();
+}
+
+get_footer();
